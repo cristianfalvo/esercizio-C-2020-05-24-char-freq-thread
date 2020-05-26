a768ccde612779e85af6acc96504f3909efd2c9f
cristianfalvo <cristianfalvo9@gmail.com>
2020-05-26 18:11:02 +0200

    started project

--- src/esercizio-C-2020-05-24-char-freq-thread.c
+++ src/esercizio-C-2020-05-24-char-freq-thread.c
@@ -2,10 +2,39 @@
  * esercizio-C-2020-05-24-char-freq-thread.c
  *
  *  Created on: May 22, 2020
- *      Author: marco
+ *      Author: utente
  */
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+#include <errno.h>
+#include <pthread.h>
+
+void * thread_function_1(void * arg);
+void * thread_function_2(void * arg);
 
 int main() {
+
+	char * content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mattis rhoncus urna neque viverra justo nec ultrices. Pretium quam vulputate dignissim suspendisse in est ante. Vitae congue mauris rhoncus aenean. Blandit cursus risus at ultrices mi. Ut lectus arcu bibendum at varius vel pharetra vel. Etiam non quam lacus suspendisse faucibus interdum posuere. Eget sit amet tellus cras adipiscing enim eu turpis egestas. Lectus magna fringilla urna porttitor rhoncus dolor purus non. Sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Nec tincidunt praesent semper feugiat nibh. Sapien pellentesque habitant morbi tristique senectus et netus et malesuada.";
+
+
+
 	return 0;
+}
+
+
+void * thread_function_1(void * arg){
+
+}
+void * thread_function_2(void * arg){
+
 }
